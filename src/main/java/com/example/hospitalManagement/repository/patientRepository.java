9d86b9845057031a851f94380af169cdb66d7194
vishu5683 <vishubhardwaj46@gmail.com>
2026-07-02 23:39:22 +0530

    added new interfaces implemented Patient.java

--- src/main/java/com/example/hospitalManagement/repository/patientRepository.java
+++ src/main/java/com/example/hospitalManagement/repository/patientRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface patientRepository extends JpaRepository<Patient, Long> { //chnged name
+public interface patientRepository extends JpaRepository<Patient, Long> {
 
 }
